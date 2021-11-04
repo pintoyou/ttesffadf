bf73f251fc29c5f2aee76e6344ebef3e62d63763
GameTokenFinance <info@gametoken.finance>
2021-11-05 01:28:37 +0300

    bla

---
+++ precache-manifest.f110e8e96f1dcca6b5b82459332580ae.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a684b5b141b4de9cd153f52937d5e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ab6dfe57ae78984f006",
+    "url": "/static/css/2.04096f9a.chunk.css"
+  },
+  {
+    "revision": "4e81b5affe01519917bc",
+    "url": "/static/css/main.a424bfc9.chunk.css"
+  },
+  {
+    "revision": "4ab6dfe57ae78984f006",
+    "url": "/static/js/2.4eb271f1.chunk.js"
+  },
+  {
+    "revision": "4e81b5affe01519917bc",
+    "url": "/static/js/main.2384090a.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  },
+  {
+    "revision": "9b8bbee93e6e7857b221af87905ebcd8",
+    "url": "/static/media/banner2.9b8bbee9.png"
+  },
+  {
+    "revision": "9fc362344d280d4c2baa67a1d241d853",
+    "url": "/static/media/bannerbg.9fc36234.png"
+  },
+  {
+    "revision": "e5ae162d4ad1b3755ddf460c57e3404f",
+    "url": "/static/media/battle-loading.e5ae162d.gif"
+  },
+  {
+    "revision": "c66df33104db5e68aa7e9f2c86a74492",
+    "url": "/static/media/bgprofile.c66df331.jpg"
+  },
+  {
+    "revision": "cb3beeaf6e963ffd730c8f2db9559195",
+    "url": "/static/media/create-loading.cb3beeaf.gif"
+  },
+  {
+    "revision": "3a985b325c8c0b2b9d75f8b1a6c5baa1",
+    "url": "/static/media/dex.3a985b32.png"
+  },
+  {
+    "revision": "758a33a89ce3f8fe863b601bca4f1e09",
+    "url": "/static/media/dex2.758a33a8.png"
+  },
+  {
+    "revision": "bafe80b404a30d91e0609973d27805ae",
+    "url": "/static/media/dex3.bafe80b4.png"
+  },
+  {
+    "revision": "1830dadf1cca57680c3badd077df6d00",
+    "url": "/static/media/hp.1830dadf.png"
+  },
+  {
+    "revision": "58e5cc777b18780c06d6fddd31cb7177",
+    "url": "/static/media/hp2.58e5cc77.png"
+  },
+  {
+    "revision": "f3f1fa6972f43fa3fc555c4b024d23a7",
+    "url": "/static/media/hp3.f3f1fa69.png"
+  },
+  {
+    "revision": "3fb887a00d3b61c41487c7d4fda9bee2",
+    "url": "/static/media/int.3fb887a0.png"
+  },
+  {
+    "revision": "a25504ad7e44d60c213b2b8091e8c27e",
+    "url": "/static/media/int2.a25504ad.png"
+  },
+  {
+    "revision": "bad3d75c79ade991f5058bc50544c669",
+    "url": "/static/media/int3.bad3d75c.png"
+  },
+  {
+    "revision": "2f0b7a20540d68a658d01ae656bfe913",
+    "url": "/static/media/mgc.2f0b7a20.png"
+  },
+  {
+    "revision": "a67064e48035ec8c31b69052c3b54166",
+    "url": "/static/media/mgc2.a67064e4.png"
+  },
+  {
+    "revision": "337b65de85cc05c8673b9fed7069d8de",
+    "url": "/static/media/mgc3.337b65de.png"
+  },
+  {
+    "revision": "b67e6ed2413c113750977f1bfd62e549",
+    "url": "/static/media/shield.b67e6ed2.png"
+  },
+  {
+    "revision": "858d99ce796ef3afe069671994e14cd3",
+    "url": "/static/media/str.858d99ce.png"
+  },
+  {
+    "revision": "dbf566ecf616dda729db19bbf9c703b6",
+    "url": "/static/media/str2.dbf566ec.png"
+  },
+  {
+    "revision": "dbe3a6ced18f2d89bdfac1b4adc74f61",
+    "url": "/static/media/str3.dbe3a6ce.png"
+  },
+  {
+    "revision": "45b0287cb8208c5a409f3d5afbea5694",
+    "url": "/static/media/warrior-icon.45b0287c.png"
+  }
+]);
